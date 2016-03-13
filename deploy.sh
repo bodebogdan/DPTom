070200c749f218687893b87a44addeaee9694e83
Adi-Vadim-Tudor <jupaddy@yahoo.com>
2016-03-13 14:25:46 +0200

    Script for site update
    Opens firefox and deletes previous .war

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ cd $HOME
 
 cd /home/$USER/workspace/CalendarOnline/WebContent
 ls
+rm -rf Site.war
 jar -cvf Site.war *
 
 cp Site.war /home/$USER/Documents/TOMCAT/webapps
@@ -13,3 +14,5 @@ cd /home/adrian/Documents/TOMCAT
 chmod 777 -R bin
 cd bin
 sh startup.sh
+
+firefox localhost:8080/Site/
